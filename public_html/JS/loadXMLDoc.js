bc5d89927f7ce1be98348c902cf3ce989354e1ea
AnthonyQuinn <AnthonyQuinn@AnthonyQuinn-PC>
2017-06-05 22:32:08 +0100

    Further changes. Added a javascript $.ajax() function.

--- public_html/JS/loadXMLDoc.js
+++ public_html/JS/loadXMLDoc.js
@@ -4,13 +4,39 @@
  * and open the template in the editor.
  */
 
-function loadXMLDoc(){
+function loadXMLDoc() {
+    console.log(' Parsing has started');
+
     var xml;
-    $(document).ready(function(){
+    $(document).ready(function () {
         $.ajax({
             type: "GET",
-            url:"XML_Files/LOLCatz.xml",
+            url: "XML_Files/LOLCatz.xml",
             dataType: "xml",
-            success: xmlParser       })
+            success: getXmlData});
+        	console.log(' Parsing has  completed');
+
     });
-});
+     };
+     function getXmlData(data){
+	xml = data;
+
+	$(xml).find("kitteh").each(function (){
+		kitteh = $(this);
+		var name = $(kitteh).find("name").text();
+		var src = $(kitteh).find("url").text();
+		var id = $(kitteh).find("id").text();
+		var url = $(kitteh).find("url").text();
+
+		$('#myList').append('<li><a href="#'+id+'">'+ name + '</a></li>');
+
+		/**$('#pageone').append(
+			'<div data-role="page" id="'+id+'"><img src="'+url+'"><a href="#pageone">Back to Page One</a></div>'); **/
+                $('#home').append(
+			'<div data-role="page" id="'+id+'"><img src="'+url+'"><a href="#home">Back to Page One</a></div>');
+
+	});
+
+}
+
+
